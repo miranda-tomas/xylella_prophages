ee81e51c8b8106a64266d839794f25b3d7c465f3
miranda-tomas <133870369+miranda-tomas@users.noreply.github.com>
2024-07-03 16:29:35 +0200

    gggenomes.R

--- gggenomes.R
+++ gggenomes.R
@@ -1,13 +1,16 @@
+# Cargar paquetes necesarios
 library(gggenomes)
 
+# Cargar los ficheros necesarios
 s0 <- read_seqs("xylella_prophages.fasta")
 g0 <- read_feats("xylella_prophages.gff") 
 l0 <- read_links("xylella_prophages.o6")
 
-colnames(l0)[4] <- "Similarity"
-
+# Paleta de colores utilizada
 pal = c("lightskyblue1", "lightpink1", "lemonchiffon", "lightsalmon1", "mistyrose2", "thistle", "lightsteelblue3", "indianred1", "bisque3", "grey")
 
+# Crear representación
+colnames(l0)[4] <- "Similarity"
 Functional_Modules <- g0$phase
 figura <- gggenomes(seqs=s0, links=l0, genes = g0) + geom_seq() + geom_link(aes(alpha=Similarity)) + geom_gene(aes(fill=Functional_Modules)) + 
   geom_bin_label(size=4, nudge_left=0.01, expand_left =0.12 )  + 
@@ -19,10 +22,11 @@ figura <- gggenomes(seqs=s0, links=l0, genes = g0) + geom_seq() + geom_link(aes(
                                                                                   'lysis',
                                                                                   'Moron, auxiliary metabolic gene and host takeover',
                                                                                   'tail',
-                                                                                  'rranscription regulation',
+                                                                                  'transcription regulation',
                                                                                   'connector',
                                                                                   'other',
                                                                                   'unknown function'))
 
+# Guardar representación en formato svg
 ggsave("gggenomes.svg", plot =figura, width = 15, height = 5, bg = "transparent")
 
